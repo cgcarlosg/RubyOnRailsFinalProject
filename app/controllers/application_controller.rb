d7dd9472b5861491be10270822796ef9fc4307c4
carlos <cgcarlosg@hotmail.com>
2021-02-14 23:23:20 -0500

    fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -73,7 +73,7 @@ class ApplicationController < ActionController::Base
   private
 
   def logged_in_user
-    unless logged_in?
+    return if logged_in?
       store_location
       flash[:danger] = 'Please log in.'
       redirect_to login_url
