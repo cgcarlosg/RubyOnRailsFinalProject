0c42a2de202c435e3a5294b9f8a24b42c84db71b
Carlos Albenio Gutierrez Monroy <cgcarlosg@hotmail.com>
2021-02-17 01:11:44 -0500

    Delete tran.rb

--- app/models/tran.rb
+++
@@ -1,8 +0,0 @@
-class Tran < ApplicationRecord
-  belongs_to :user
-  belongs_to :category, optional: true
-  has_many :categories
-  validates :title, presence: true, uniqueness: { case_sensitive: false }
-  validates :total, presence: true
-  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
-end
