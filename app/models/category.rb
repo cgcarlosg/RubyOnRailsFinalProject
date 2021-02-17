7913401fa3744678676026114ae1297a33e330d5
Carlos Albenio Gutierrez Monroy <cgcarlosg@hotmail.com>
2021-02-17 01:12:15 -0500

    Delete category.rb

--- app/models/category.rb
+++
@@ -1,7 +0,0 @@
-class Category < ApplicationRecord
-  belongs_to :user
-  has_many :trans, dependent: :destroy
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :icon, presence: true
-  scope :alphabetical_order, -> { order(name: :asc) }
-end
